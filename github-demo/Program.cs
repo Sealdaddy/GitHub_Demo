f306b2e30ca2f08edbb6e69f39d65e03585e6ae9
Adam Seal <aseal33@gmail.com>
2018-10-24 10:19:46 -0400

    Changed Message

--- github-demo/Program.cs
+++ github-demo/Program.cs
@@ -6,7 +6,7 @@ namespace github_demo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Cohort [9]!");
         }
     }
 }
